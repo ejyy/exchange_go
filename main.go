2e6620df2f8fba54784254eca6b740ef2ad206d0
Elliot Yates <elliotyatesj@gmail.com>
2024-08-15 15:43:09 +0100

    Correct stock symbols

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ func main() {
 	var exchange Exchange
 	exchange.Init("Example exchange")
 
-	test_order := Order{symbol: "APPL", price: 100, size: 1000, side: Bid, trader: 1}
+	test_order := Order{symbol: "AAPL", price: 100, size: 1000, side: Bid, trader: 1}
 	exchange.Limit(test_order)
 
-	test_order2 := Order{symbol: "APPL", price: 100, size: 1000, side: Ask, trader: 2}
+	test_order2 := Order{symbol: "AAPL", price: 100, size: 1000, side: Ask, trader: 2}
 	exchange.Limit(test_order2)
 
-	test_order3 := Order{symbol: "GOOG", price: 100, size: 1000, side: Bid, trader: 1}
+	test_order3 := Order{symbol: "GOOGL", price: 100, size: 1000, side: Bid, trader: 1}
 	exchange.Limit(test_order3)
 
-	test_order4 := Order{symbol: "GOOG", price: 100, size: 1000, side: Ask, trader: 2}
+	test_order4 := Order{symbol: "GOOGL", price: 100, size: 1000, side: Ask, trader: 2}
 	exchange.Limit(test_order4)
 
 	// exchange.Cancel("APPL", 1)
