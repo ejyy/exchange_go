6d119ef3e10603b457d31b2210ca2f679f510f8b
Elliot Yates <elliotyatesj@gmail.com>
2024-08-15 15:35:38 +0100

    Always forget to go fmt, need to trigger this

--- actions.go
+++ actions.go
@@ -1,67 +1,68 @@
 package main
 
 type ActionType string
+
 const (
-	ACTION_BID = "BID"
-	ACTION_ASK = "ASK"
-	ACTION_CANCEL = "CANCEL"
+	ACTION_BID     = "BID"
+	ACTION_ASK     = "ASK"
+	ACTION_CANCEL  = "CANCEL"
 	ACTION_EXECUTE = "EXECUTION"
 )
 
 type Action struct {
-	action_type ActionType
-	symbol string
-	order_id OrderID
+	action_type    ActionType
+	symbol         string
+	order_id       OrderID
 	order_id_other OrderID
-	price Price
-	size Size
-	trader TraderID
-	trader_other TraderID
+	price          Price
+	size           Size
+	trader         TraderID
+	trader_other   TraderID
 }
 
 func newBidAction(order *Order) *Action {
-    return &Action{
+	return &Action{
 		action_type: ACTION_BID,
-		symbol: order.symbol,
-		order_id: order.order_id,
-		price: order.price,
-		size: order.size,
-		trader: order.trader,
+		symbol:      order.symbol,
+		order_id:    order.order_id,
+		price:       order.price,
+		size:        order.size,
+		trader:      order.trader,
 	}
 }
 
 func newAskAction(order *Order) *Action {
-    return &Action{
+	return &Action{
 		action_type: ACTION_ASK,
-		symbol: order.symbol,
-		order_id: order.order_id,
-		price: order.price,
-		size: order.size,
-		trader: order.trader,
+		symbol:      order.symbol,
+		order_id:    order.order_id,
+		price:       order.price,
+		size:        order.size,
+		trader:      order.trader,
 	}
 }
 
 func newCancelAction(order *Order) *Action {
-    return &Action{
+	return &Action{
 		action_type: ACTION_CANCEL,
-		symbol: order.symbol,
-		order_id: order.order_id,
-		price: order.price,
-		size: order.size,
-		trader: order.trader,
+		symbol:      order.symbol,
+		order_id:    order.order_id,
+		price:       order.price,
+		size:        order.size,
+		trader:      order.trader,
 	}
 }
 
 func newExecuteAction(order *Order, entry *Order) *Action {
 	// If statement to report execution based on side
-    return &Action{
-		action_type: ACTION_EXECUTE,
-		symbol: order.symbol,
-		order_id: order.order_id, // Change this around?
+	return &Action{
+		action_type:    ACTION_EXECUTE,
+		symbol:         order.symbol,
+		order_id:       order.order_id, // Change this around?
 		order_id_other: entry.order_id, // Change this around?
-		price: entry.price,
-		size: order.size, // Change this around?
-		trader: order.trader, // Change this around?
-		trader_other: entry.trader, // Change this around?
+		price:          entry.price,
+		size:           order.size,   // Change this around?
+		trader:         order.trader, // Change this around?
+		trader_other:   entry.trader, // Change this around?
 	}
 }
