8751e9632fcd882ff6a02503dece1033d5c2d5f8
Elliot Yates <elliotyatesj@gmail.com>
2024-08-16 21:20:12 +0100

    Added tests for exchange

--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -5,6 +5,87 @@ import (
 	"testing"
 )
 
+func TestExchange_Init(t *testing.T) {
+	actions := make(chan *Action, CHAN_SIZE)
+	var exchange Exchange
+	exchange.Init("Test Exchange", actions)
+
+	if exchange.name != "Test Exchange" {
+		t.Errorf("Expected exchange name to be 'Test Exchange', got %s", exchange.name)
+	}
+	if exchange.current_order_id != 0 {
+		t.Errorf("Expected current_order_id to be 0, got %d", exchange.current_order_id)
+	}
+	if len(exchange.orderbooks_map) != 0 {
+		t.Errorf("Expected orderbooks_map to be empty, got %d", len(exchange.orderbooks_map))
+	}
+	if len(exchange.order_id_map) != 0 {
+		t.Errorf("Expected order_id_map to be empty, got %d", len(exchange.order_id_map))
+	}
+	if exchange.actions != actions {
+		t.Errorf("Expected actions channel to be set")
+	}
+}
+
+func TestExchange_getOrCreateOrderBook(t *testing.T) {
+	actions := make(chan *Action, CHAN_SIZE)
+	var exchange Exchange
+	exchange.Init("Test Exchange", actions)
+
+	symbol := "AAPL"
+	orderBook := exchange.getOrCreateOrderBook(symbol)
+
+	if orderBook == nil {
+		t.Errorf("Expected order book to be created")
+	}
+	if exchange.orderbooks_map[symbol] != orderBook {
+		t.Errorf("Expected order book to be stored in orderbooks_map")
+	}
+}
+
+func TestExchange_Limit(t *testing.T) {
+	actions := make(chan *Action, CHAN_SIZE)
+	var exchange Exchange
+	exchange.Init("Test Exchange", actions)
+
+	symbol := "AAPL"
+	price := Price(100)
+	size := Size(10)
+	side := Bid
+	trader := TraderID(1)
+
+	exchange.Limit(symbol, price, size, side, trader)
+
+	orderBook := exchange.getOrCreateOrderBook(symbol)
+	if len(orderBook.price_points) == 0 {
+		t.Errorf("Expected order to be added to the order book")
+	}
+	if orderBook.bid_max != 100 {
+		t.Errorf("Expected bid_max to be 100 after adding the order")
+	}
+}
+
+func TestExchange_Cancel(t *testing.T) {
+	actions := make(chan *Action, CHAN_SIZE)
+	var exchange Exchange
+	exchange.Init("Test Exchange", actions)
+
+	symbol := "AAPL"
+	price := Price(100)
+	size := Size(10)
+	side := Bid
+	trader := TraderID(1)
+
+	exchange.Limit(symbol, price, size, side, trader)
+	orderID := exchange.current_order_id
+
+	exchange.Cancel(orderID)
+
+	if exchange.order_id_map[orderID].size != 0 {
+		t.Errorf("Expected order size to be 0 after cancellation")
+	}
+}
+
 // go test -bench=BenchmarkExchange
 func BenchmarkExchange(b *testing.B) {
 	minSize := 1
