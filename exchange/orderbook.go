d40fce48f6687fcf6954fbed19daa99f8dc7df91
Elliot Yates <elliotyatesj@gmail.com>
2025-08-15 08:18:39 +0100

    Smaller initialisation btree for improved performance

--- exchange/orderbook.go
+++ exchange/orderbook.go
@@ -37,8 +37,8 @@ func (ob *OrderBook) init(symbol string, exchange *Exchange) {
 	ob.symbol = symbol
 	ob.exchange = exchange
 
-	ob.asks = btree.New(int(MaxPrice))
-	ob.bids = btree.New(int(MaxPrice))
+	ob.asks = btree.New(32)
+	ob.bids = btree.New(32)
 }
 
 // limitHandle processes an incoming order in the following manner:
